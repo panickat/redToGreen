f5113d135cafc3300ea90dad8be06817db860827
panic kat <krizkrag@gmail.com>
2023-02-25 14:57:50 -0600

    copycat ok

--- scraper/src/App.vue
+++ scraper/src/App.vue
@@ -1,10 +1,21 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
   <HelloWorld msg="from app vue" />
+  <div>
+    <h1>Web Scraper</h1>
+    <div class="wrapper">
+      <div v-for="(article, index) in lastestArticles" :key="index">
+        <span v-text="article.title"></span>
+        <hr />
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 import HelloWorld from "./components/HelloWorld.vue";
+import axios from "axios";
+import cheerio from "cheerio";
 
 export default {
   name: "App",
@@ -15,6 +26,38 @@ export default {
     return {
       lastestArticles: [], // defined empty array variable
     };
+  },
+  methods: {
+    getWebsiteData() {
+      let self = this;
+      let url = "https://www.msn.com/en-au?AR=1"; // url we get data from
+      let dataArray = []; // we put data in this array
+      // GET request for remote image in node.js
+      axios({
+        method: "get",
+        url: url,
+      }).then(function (response) {
+        let html = response.data;
+
+        let $ = cheerio.load(html);
+
+        $("ul.tertiary li").each(function () {
+          const title = $(this).find("h3").attr("aria-label");
+          const image = $(this).find("img").attr("src");
+
+          // putting data in array.
+
+          dataArray.push({
+            title: title,
+            image: image,
+          });
+        });
+
+        self.lastestArticles = dataArray; // Here we assign value to vuejs variable
+
+        console.log(dataArray);
+      });
+    },
   },
 };
 </script>

b985513b6bf9df2635f79162ad8b36125a9baadc
panic kat <krizkrag@gmail.com>
2023-03-15 17:24:24 -0600

    main url proxy ok, 1st brower test ok, 2nd file on host error

--- scraper/testProxy/middleware.js
+++ scraper/testProxy/middleware.js
@@ -1,18 +1,15 @@
 const express = require("express");
 const { createProxyMiddleware } = require("http-proxy-middleware");
 const cors = require("cors");
-// const myUserAgent =
-//   "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36";
+const myUserAgent =
+  "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_8) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36";
 
 const app = express();
 const secrets = require("./secrets.json");
 
-// const remoteOpenprovider = secrets.remoteOpenprovider;
-// const remoteCloudflare = secrets.remoteCloudflare;
-
-// function hostName(remoteSite) {
-//   return remoteSite.replace(/^https?:\/\//i, "");
-// }
+function hostName(remoteSite) {
+  return remoteSite.replace(/^https?:\/\//i, "");
+}
 
 // function setHeaders(res, path) {
 //   if (/\.(gif|jpg|jpeg|png|th.jpg)$/i.test(path)) {
@@ -80,7 +77,6 @@ function setOptions(target, oldPath, newPath) {
     },
   };
 }
-
 const searchOptions = setOptions(
   secrets.remoteSearch,
   "/proxy/search",
@@ -93,7 +89,7 @@ const downloadOptions = setOptions(secrets.remoteOpen, "/proxy/open", "/");
 const downloadProxy = createProxyMiddleware(downloadOptions);
 app.use("/proxy/open", downloadProxy);
 
-// err
+// ####################### err  #######################
 const corsOptions = {
   origin: "*", // allow requests from any origin
   methods: "GET, POST, PUT, DELETE",
@@ -107,23 +103,60 @@ const corsOptions = {
 };
 app.use(cors(corsOptions));
 
-const cdncgiOptions = setOptions(secrets.remoteCloud, "/cdn-cgi", "/cdn-cgi");
+function setcdncgiOptions(target, oldPath, newPath) {
+  /** @type {import('http-proxy-middleware/dist/types').Options} */
+  return {
+    target, // target host
+    changeOrigin: true, // needed for virtual hosted sites
+    ws: true, // proxy websockets
+    pathRewrite: {
+      [`^${oldPath}`]: newPath, // rewrite path
+    },
+    onProxyReq(proxyReq, req, res) {
+      proxyReq.setHeader(
+        "Cookie",
+        "cf_chl_rc_i=11; Domain=downl.ink; Path=/; cf_clearance=hemA_E7RMkXmPiDrAKrywHw17oZDNeCUaEAVWDXzGBc-1678907476-0-250; Domain=.downl.ink; Path=/;"
+      );
+    },
+    headers: {
+      Accept:
+        "text/html,txt/javascript,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8",
+      "Accept-Encoding": "gzip, deflate",
+      "Accept-Language": "en-US,en;q=0.9",
+      "Cache-Control": "no-cache",
+      Connection: "keep-alive",
+      Host: "downl.ink",
+      Pragma: "no-cache",
+      Referer: target,
+      "Sec-GPC": 1,
+      "Upgrade-Insecure-Requests": 1,
+      "User-Agent": myUserAgent,
+    },
+  };
+}
+// ## main url
+const cloudOptions = setcdncgiOptions(secrets.remoteCloud, "/proxy/cloud", "/");
+const cloudProxy = createProxyMiddleware(cloudOptions);
+app.use("/proxy/cloud", cloudProxy);
+
+// ## 1st browser test
+const cdncgiOptions = setcdncgiOptions(
+  secrets.remoteCloud,
+  "/cdn-cgi",
+  "/cdn-cgi"
+);
 const cdncgiProxy = createProxyMiddleware(cdncgiOptions);
 app.use("/cdn-cgi", cdncgiProxy);
 
-const cloudOptions = setOptions(secrets.remoteCloud, "/proxy/cloud", "/");
-const cloudProxy = createProxyMiddleware(cloudOptions);
-app.use("/proxy/cloud", cloudProxy);
-// err
-//test
-const totalOptions = setOptions(
-  secrets.remoteTotal,
-  "/proxy/total",
-  "/archivos/pdf"
+// 2nd file on host
+const fileOptions = setcdncgiOptions(
+  secrets.remoteCloud,
+  "/d7cca1f85",
+  "/d7cca1f85"
 );
-const totalProxy = createProxyMiddleware(totalOptions);
-app.use("/proxy/total", totalProxy);
-// end test
+const fileProxy = createProxyMiddleware(fileOptions);
+app.use("/d7cca1f85", fileProxy);
+// ####################### err  #######################
 
 app.use((req, res, next) => {
   console.log(`## req.url: ${req.url}`);
@@ -146,3 +179,13 @@ process.on("SIGINT", function () {
   );
   process.exit(0);
 });
+
+// //test total
+// const totalOptions = setOptions(
+//   secrets.remoteTotal,
+//   "/proxy/total",
+//   "/archivos/pdf"
+// );
+// const totalProxy = createProxyMiddleware(totalOptions);
+// app.use("/proxy/total", totalProxy);
+// // end test total
